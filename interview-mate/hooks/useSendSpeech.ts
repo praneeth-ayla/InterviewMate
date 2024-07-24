bc7b39ae2735fb78d43806b569ce08c035bf7e61
Praneeth <praneethayla@gmail.com>
2024-07-24 16:39:49 +0530

    deploy test

--- interview-mate/hooks/useSendSpeech.ts
+++ interview-mate/hooks/useSendSpeech.ts
@@ -2,6 +2,7 @@ export function useSendSpeech() {
 	function sendWS({ text, role }: { text: string; role: string }) {
 		if (text !== "") {
 			// send to websocket
+			console.log("data:", { text, role });
 		}
 	}
 
